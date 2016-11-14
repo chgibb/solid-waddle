ccc8da4a0bcfbd0c7551af497082d438218db2ac
ErikTillberg <eatillberg@gmail.com>
2016-11-14 10:20:00 -0500

    reply to tickets and view the replies

--- src/client/views/dashboard/inbox/inbox.js
+++ src/client/views/dashboard/inbox/inbox.js
@@ -1,5 +1,8 @@
 Template.inbox.helpers({
   announcement: function(){
     return Announcements.find();
+  },
+  replyObject: function(){
+    return Tickets.find({$and: [{senderID: Meteor.userId()}, {reply: {$exists: true}}]});
   }
 });
